2c480768b3437f9646b6aa841c203558012202db
=lFloriano <lwcas.franca@gmail.com>
2019-05-27 22:54:56 -0300

    Script populacao tbl: add insert de projetos

--- Script populacao tabela.sql
+++ Script populacao tabela.sql
@@ -89,3 +89,18 @@ insert into Colaboradores Values
 (getdate(), 'Genghis Kahn', 'Assistente de comunicação', 'Marketing', getdate()),
 (getdate(), 'Napoleão Bonaparte', 'Gerente de Projeto', 'Inovação', getdate()),
 (getdate(), 'Poucas Trancas', 'Assistente de comunicação', 'Marketing', getdate())
+
+-------PROJETOS---------------
+insert into projetos(
+	criado, nome, Descricao, Responsavel, DataInicio, DataFim, idcliente, statusProjeto)
+values
+	(getdate(), 'Projeto A','Exemplo de texto com a descrição do projeto', 1, '2019-05-04', '2019-09-04', 1, 'Em andamento'),
+	(getdate(), 'Projeto B','Exemplo de texto com a descrição do projeto', 1, '2019-01-14', '2019-09-04', 4, 'Em andamento'),
+	(getdate(), 'Projeto C','Exemplo de texto com a descrição do projeto', 2, '2019-05-04', '2020-02-11', 5, 'Pausado'),
+	(getdate(), 'Projeto D','Exemplo de texto com a descrição do projeto', 2, '2020-11-04', '2022-09-22', 2, 'Em Planejamento'),
+	(getdate(), 'Projeto E','Exemplo de texto com a descrição do projeto', 3, '2019-05-04', '2019-09-04', 5, 'Pausado'),
+	(getdate(), 'Projeto F','Exemplo de texto com a descrição do projeto', 3, '2019-05-04', '2019-09-04', 4, 'Em andamento'),
+	(getdate(), 'Projeto G','Exemplo de texto com a descrição do projeto', 4, '2019-08-04', '2019-12-12', 1, 'Em andamento'),
+	(getdate(), 'Projeto H','Exemplo de texto com a descrição do projeto', 4, '2019-01-04', '2019-10-14', 3, 'Pausado'),
+	(getdate(), 'Projeto I','Exemplo de texto com a descrição do projeto', 5, '2019-01-01', '2019-07-29', 4, 'Em andamento'),
+	(getdate(), 'Projeto J','Exemplo de texto com a descrição do projeto', 5, '2019-02-02', '2019-10-22', 3, 'Em andamento')
